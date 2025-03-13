c66cd0ec778a3da0b5c2354c2eceb71c4d626fc4
KevalThumarInventyv <keval.thumar.inventyv@gmail.com>
2025-03-13 18:51:47 +0530

    upto settingPage done

---
+++ HealthCoach/Controller/Home/SideMenu/SideMenuManager.swift
@@ -0,0 +1,19 @@
+//
+//  SideMenuViewController.swift
+//  HealthCoach
+//
+//  Created by Keval Thumar on 12/03/25.
+//
+
+import UIKit
+
+class SideMenuViewController: HomeViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
