7169554fc041e2b4a181113a327e41025b92bf2d
KevalThumarInventyv <keval.thumar.inventyv@gmail.com>
2025-03-19 17:32:22 +0530

    Work on setting page

---
+++ HealthCoach/Controller/Settings/CheckBoxTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CheckBoxTableViewCell.swift
+//  HealthCoach
+//
+//  Created by Keval Thumar on 19/03/25.
+//
+
+import UIKit
+
+class CheckBoxTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ HealthCoach/Utilities/Validation.swift
@@ -0,0 +1,8 @@
+//
+//  Validation.swift
+//  HealthCoach
+//
+//  Created by Keval Thumar on 19/03/25.
+//
+
+import Foundation
